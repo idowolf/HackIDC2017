9b3d055203652275a612eb135449f9a0361aed38
idowolf <idowolf@gmail.com>
2017-04-28 05:21:10 +0300

    added shooting bullets

---
+++ Assets/BulletFactoryLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFactoryLogic : MonoBehaviour {
+    public GameObject bullet;
+    private float timeElapsed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+        if(timeElapsed >= 0.5f)
+        {
+            timeElapsed = 0;
+            GameObject.Instantiate(bullet);
+        }
+	}
+}
--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hitByBullet : MonoBehaviour {
-
+public class MoveBullet : MonoBehaviour {
+    public float velocity = 0.1f;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	void OnCollisionEnter(Collision col) {
-		Debug.Log("test");
-	}
-
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 pos = transform.position;
+        pos.z -= velocity*Time.deltaTime;
+        transform.position = pos;
 	}
 }
