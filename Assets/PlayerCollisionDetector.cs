1a86f145ad3a6eb40273981836d75c2b0b48c434
elmerj <nimrodmic@gmail.com>
2017-04-28 06:05:26 +0300

    added camera back plain, player hp

--- Assets/PlayerCollisionDetector.cs
+++ Assets/PlayerCollisionDetector.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hitByBullet : MonoBehaviour {
+public class PlayerCollisionDetector : MonoBehaviour {
 
 	// Use this for initialization
+	public int hp = 100;
 	void Start () {
 		
 	}
-	
-	void OnTriggerEnter(Collider col) {
-		GameObject.Destroy(gameObject);
+		void OnTriggerEnter(Collider col) {
+			hp -= 10;
 	}
-
 	// Update is called once per frame
 	void Update () {
 		
